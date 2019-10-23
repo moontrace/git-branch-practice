68ec410a4e15f0d87d651f1524c29d7d769d98e5
Jong in Kim <moontrace@gmail.com>
2019-10-23 15:34:50 +0900

    feat: add condtional statements
    Added if statements some numbers and print string

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-# utf-8
-print("Python works!")
+i = int(input("type some numbers(1 to 100) : "))
 
+if i % 15 == 0:
+    print("fizbuzz")
+elif i % 3 == 0:
+    print("fizz")
+elif i % 5 == 0:
+    print("buzz")
+else:
+    print(i)
